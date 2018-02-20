37a59c9941b1de7566f86ced22de009849ebb964
jinming-zhang <jinming.zhang47@gmail.com>
2018-02-20 16:47:34 -0500

    d

--- A2/A2DPLL.hs
+++ A2/A2DPLL.hs
@@ -8,4 +8,62 @@ data Atom = Pos String | Neg String deriving Show
 
 -- True if satisfiable, False if unsatisfiable.
 cnfSAT :: [[Atom]] -> Bool
-cnfSAT = error "TODO"
+cnfSAT [[]] = False
+cnfSAT [[],_] = False
+cnfSAT [] = True
+-- return the result of try set the first variable to true or false
+cnfSAT lst =  (cnfSAT (simplify lst (Pos (getFirst (head (head lst)))))) || (cnfSAT (simplify lst (Neg (getFirst (head (head lst))))))
+
+
+
+
+getFirst :: Atom -> String
+getFirst (Pos var) = var
+getFirst (Neg var) = var
+
+
+
+
+-- simplifies the givin list of list of Atom according to givin Atom
+-- if Atom is of (Pos var), then remove all the [Atom] that contains (Pos var)
+--                          and remove all the (Neg var) in each [Atom]
+-- if Atom is of (Neg var), then remove all the [Atom] that contains (Neg var)
+--                          and remove all the (Pos var) in each [Atom]
+simplify :: [[Atom]] -> Atom -> [[Atom]]
+-- treat the given var as true
+simplify lst (Pos var)
+        | length lst == 0 = []
+        -- remove all clauses that contains (Pos var)
+        | length [ (Pos var1) | (Pos var1) <- (head lst), var1 == var] /= 0 = simplify (tail lst) (Pos var)
+        -- for each clause that doesn't contain (Pos var), check if it contains (Neg var)
+        -- if yes then remove (Neg var) from the elmt list
+        | length [(Neg var2) | (Neg var2) <- (head lst), var2 == var] /= 0 = [removeAtom (head lst) (Neg var)] ++ simplify (tail lst) (Pos var)
+        | otherwise = [(head lst)] ++ simplify (tail lst) (Pos var)
+-- treat the given var as false
+simplify lst (Neg var)
+        | length lst == 0 = []
+        -- remove all clauses that contains (Neg var)
+        | length [ (Neg var1) | (Neg var1) <- (head lst), var1 == var] /= 0 = simplify (tail lst) (Neg var)
+        -- for each clause that doesn't contain (Neg var), check if it contains (Pos var)
+        -- if yes then remove (Pos var) from the [Atom]
+        | length [(Pos var2) | (Pos var2) <- (head lst), var2 == var] /= 0 = [removeAtom (head lst) (Pos var)] ++ simplify (tail lst) (Neg var)
+        | otherwise = [(head lst)] ++ simplify (tail lst) (Neg var)
+
+
+
+-- removes the givin Atom in the given [Atoms]
+-- to remove a (Pos var), then extract every (Neg var), and (Pos var2) which var2 != var 
+-- to remove a (Neg var), do the opposite
+removeAtom :: [Atom] -> Atom -> [Atom]
+removeAtom lst (Neg var) = [(Pos var1) | (Pos var1) <- lst] 
+                       ++  [(Neg var2) | (Neg var2) <- lst, var2 /= var]
+removeAtom lst (Pos var) = [(Neg var1) | (Neg var1) <- lst] 
+                       ++  [(Pos var2) | (Pos var2) <- lst, var2 /= var]                       
+
+
+
+
+
+
+--let u = [ [Pos "x", Pos "y", Pos "z"], [Pos "x", Pos "y", Neg "z"], [Pos "x", Neg "y", Pos "z"], [Pos "x", Neg "y", Neg "z"], [Neg "x", Pos "y", Pos "z"], [Neg "x", Pos "y", Neg "z"], [Neg "x", Neg "y", Pos "z"], [Neg "x", Neg "y", Neg "z"]]
+--let a = [[Pos "x", Pos "y", Pos "z"], [Pos "x", Pos "y", Neg "z"]]
--- A2/A2Trie.hs
+++ A2/A2Trie.hs
@@ -5,7 +5,7 @@ import           Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IntMap
 
 -- It's OK to import stuff from the standard library. Put your import lines here.
-
+import           Data.Maybe
 -- The following two utility functions will help you.  They treat IntMap as a
 -- map from Char.
 charMapLookup :: Char -> IntMap a -> Maybe a
@@ -19,7 +19,48 @@ data Trie a = TrieOf (Maybe a) (IntMap (Trie a))
     deriving Show
 
 trieLookup :: [Char] -> Trie a -> Maybe a
-trieLookup = error "TODO"
+-- if reaches the end of [char], the value we want is in the root
+trieLookup [] (TrieOf value  map) = value
+trieLookup (char : str) (TrieOf value  map)
+    | (ord char) `elem` (IntMap.keys map) = trieLookup str (getMap char map)
+    | otherwise = Nothing
+
+-- get the map returned by lookup
+getMap :: Char -> IntMap (Trie a) -> Trie a
+getMap char map = snd tree 
+    where 
+        tree = lst !! 0
+            where 
+                lst = [(c, m) | (c, m) <- (IntMap.toList map), c == (ord char)]
+
+
 
 trieInsert :: [Char] -> a -> Trie a -> Trie a
-trieInsert = error "TODO"
+-- nomore char to insert, create the Trie of just the value
+trieInsert [] value (TrieOf oldv oldm) = (TrieOf (Just value) oldm)
+
+trieInsert (char : str) value (TrieOf oldv oldm)
+    -- head character is in the dictionary
+    | (isJust (charMapLookup char oldm)) = (TrieOf oldv (charMapInsert char (trieInsert str value (fromJust (charMapLookup char oldm))) oldm))
+        --where
+            --newm = (charMapInsert char (trieInsert str value (fromJust (charMapLookup char oldm))) oldm)
+    -- if not, insert it to the dictionay
+    | otherwise = (TrieOf oldv newm)
+        where
+            newm = (charMapInsert char (trieInsert str value (TrieOf (Nothing) IntMap.empty)) oldm)
+
+
+
+at = TrieOf Nothing (IntMap.fromList [(ord 'p', p), (ord 't', t)])
+  where
+    p = TrieOf Nothing (IntMap.fromList [(ord 'i', pi)])
+      where
+        pi = TrieOf (Just 1) (IntMap.fromList [(ord 't', pit)])
+          where
+            pit = TrieOf (Just 9) IntMap.empty
+    t = TrieOf Nothing (IntMap.fromList [(ord 'o', to)])
+      where
+        to = TrieOf Nothing (IntMap.fromList [(ord 'p', top), (ord 'n', ton)])
+          where
+            top = TrieOf (Just 5) IntMap.empty
+            ton = TrieOf (Just 7) IntMap.empty
