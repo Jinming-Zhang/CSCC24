538bcaa8e063ddeff1063577169613a9ab34c8d1
jinming-zhang <jinming.zhang47@gmail.com>
2018-02-22 21:53:56 -0500

    d

--- A3/A3Winning.hs
+++ A3/A3Winning.hs
@@ -3,7 +3,8 @@ module A3Winning where
 import Data.Array
 
 -- You can import other modules/functions here.
-
+import Data.Maybe
+import Data.List
 data XO = X | O deriving (Eq, Show)
 
 -- A game board is represented by a 2D array.  Indexes are from (0,0)
@@ -40,9 +41,95 @@ printBoard board = do
 -- Compute all legal moves for the given board.
 -- In the answers, (i, j, X) means "put an X at cell (i,j)" for example.
 moves :: Array (Int, Int) (Maybe XO) -> [(Int, Int, XO)]
-moves = error "TODO"
+-- call the helper function with this board, its list of indeces and start from 0
+moves board 
+    | winning board = []
+    | drawing board = []
+    | otherwise = helpMoves board list 0
+    where list = indices board
+
+-- takes a gameboard, a list of its indices and a starting index
+-- return a list of all possible moves of the gameboard
+helpMoves :: Array (Int, Int) (Maybe XO) -> [(Int, Int)] -> Int -> [(Int, Int, XO)]
+helpMoves board list i
+    | i == bound = []
+    | isNothing symbol =  [(x, y, O), (x, y, X)] ++ helpMoves board list (i+1)
+    | otherwise = helpMoves board list (i+1)
+    where bound = length list
+          x = fst (list !! i)
+          y = snd (list !! i)
+          symbol = board ! (x, y)
 
 -- Compute all blowing moves for the given game board.
 -- In the answers, (i, j, X) means "put an X at cell (i,j)" for example.
 howToWin :: Array (Int, Int) (Maybe XO) -> [(Int, Int, XO)]
-howToWin = error "TODO"
+howToWin board = [mv | mv <- allMove, isBlowingMove board mv]
+    where allMove = moves board
+
+isBlowingMove :: Array (Int, Int) (Maybe XO) -> (Int, Int, XO) ->Bool
+isBlowingMove board move
+    | winning (next board move) = True
+    | otherwise = let newBoard = next board move
+                      newMoves = moves newBoard
+                  in  hasBlowingMove newBoard newMoves-- 0
+
+hasBlowingMove :: Array (Int, Int) (Maybe XO) -> [(Int, Int, XO)] -> Bool--Int -> Bool
+hasBlowingMove oppBoard oppMoves --removing
+    | [mv | mv <- oppMoves, winning (next oppBoard mv)] /= [] = False
+    | otherwise = not (null [hasBlowingMove myBoard (moves myBoard) | myBoard <- myBoards])
+    where myBoards = [ next oppBoard mv | mv <- oppMoves]
+
+-- make a next move regarding to givin player and position and gameboard
+next :: Array (Int, Int) (Maybe XO) -> (Int, Int, XO) -> Array (Int, Int) (Maybe XO)
+next board (x, y, X) = board // [ ((x, y), Just X)]
+next board (x, y, O) = board // [ ((x, y), Just O)]
+
+drawing :: Array (Int, Int) (Maybe XO) -> Bool
+drawing board = (not (winning board)) && isFull
+    where isFull = not (elem Nothing (elems board))
+
+-- Givin a symbol at certain position of the board
+-- check if that symbol is winning in the current state
+winning :: Array (Int, Int) (Maybe XO) -> Bool
+winning board = helpWinning board (indices board)
+
+
+
+
+
+
+
+
+
+
+
+
+helpWinning :: Array (Int, Int) (Maybe XO) -> [(Int, Int)] -> Bool
+helpWinning board [] = False
+helpWinning board (hd: tl) = (isWinningAt board hd) || (helpWinning board tl)
+
+-- Check if the given position of the givin board is givin symbol
+isPlayer :: Array (Int, Int) (Maybe XO) -> (Maybe XO) -> (Int, Int) -> Bool
+-- if position out of bound, then false
+isPlayer board player position
+    | not (elem position (indices board)) = False
+    | isNothing player = False
+    | (board ! position) == player = True
+    | otherwise = False
+
+isWinningAt :: Array (Int, Int) (Maybe XO) -> (Int, Int) -> Bool
+isWinningAt board (x, y)
+    | (isPlayer board currentPlayer top) && (isPlayer board currentPlayer buttom) = True
+    | (isPlayer board currentPlayer left) && (isPlayer board currentPlayer right) = True
+    | (isPlayer board currentPlayer topleft) && (isPlayer board currentPlayer buttomright) = True
+    | (isPlayer board currentPlayer topright) && (isPlayer board currentPlayer buttomleft) = True
+    | otherwise = False
+    where currentPlayer = board ! (x, y)
+          top =        (x - 1, y)
+          buttom =     (x + 1, y)
+          left =       (x, y -1)
+          right =      (x, y + 1)
+          topleft =    (x - 1, y - 1)
+          topright =   (x - 1, y + 1)
+          buttomleft = (x + 1, y - 1)
+          buttomright =(x + 1, y + 1)
